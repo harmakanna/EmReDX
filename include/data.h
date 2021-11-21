457fab6b26ac7ba3280eac9680c6279c70f31a45
harmakanna <72272517+harmakanna@users.noreply.github.com>
2021-11-20 21:28:35 -0500

    Adjusted max EVs, added expanded pokemon customizability for trainer mons with custom movesets, fixed Steven's mon's nature glitch, removed gym leaders from match call.
    -Adjusted max EVs
    -Added expanded pokemon customizability for trainer mons with custom movesets
    -Fixed Steven's mon's nature glitch
    -Removed gym leaders from match call.

--- include/data.h
+++ include/data.h
@@ -33,7 +33,10 @@ struct TrainerMonNoItemCustomMoves
 {
     u16 iv;
     u8 lvl;
+    u8 evs[NUM_STATS];
     u16 species;
+    u8 ability;
+    u8 nature;
     u16 moves[MAX_MON_MOVES];
 };
 
@@ -41,8 +44,11 @@ struct TrainerMonItemCustomMoves
 {
     u16 iv;
     u8 lvl;
+    u8 evs[NUM_STATS];
     u16 species;
     u16 heldItem;
+    u8 ability;
+    u8 nature;
     u16 moves[MAX_MON_MOVES];
 };
 
--- src/match_call.c
+++ src/match_call.c
@@ -1000,8 +1000,8 @@ static bool32 CheckMatchCallChance(void)
     int callChance = 1;
     if (!GetMonData(&gPlayerParty[0], MON_DATA_SANITY_IS_EGG) && GetMonAbility(&gPlayerParty[0]) == ABILITY_LIGHTNING_ROD)
         callChance = 2;
-    
-    if (Random() % 10 < callChance * 3) 
+
+    if (Random() % 10 < callChance * 3)
         return TRUE;
     else
         return FALSE;
@@ -1011,7 +1011,7 @@ static bool32 MapAllowsMatchCall(void)
 {
     if (!Overworld_MapTypeAllowsTeleportAndFly(gMapHeader.mapType) || gMapHeader.regionMapSectionId == MAPSEC_SAFARI_ZONE)
         return FALSE;
-    
+
     if (gMapHeader.regionMapSectionId == MAPSEC_SOOTOPOLIS_CITY
      && FlagGet(FLAG_HIDE_SOOTOPOLIS_CITY_RAYQUAZA) == TRUE
      && FlagGet(FLAG_NEVER_SET_0x0DC) == FALSE)
@@ -1477,7 +1477,7 @@ static const struct MatchCallText *GetBattleMatchCallText(int matchCallId, u8 *s
 {
     int mask;
     u32 textId, topic, id;
-    
+
     topic = Random() % 3;
     textId = sMatchCallTrainers[matchCallId].battleTopicTextIds[topic];
     if (!textId)
@@ -1742,7 +1742,7 @@ static void PopulateBattleFrontierStreak(int matchCallId, u8 *destStr)
         streak /= 10;
         i++;
     }
-    
+
     ConvertIntToDecimalStringN(destStr, gBattleFrontierStreakInfo.streak, STR_CONV_MODE_LEFT_ALIGN, i);
 }
 
@@ -1924,7 +1924,7 @@ static u8 GetPokedexRatingLevel(u16 numSeen)
         return 18;
     if (numSeen < 200)
         return 19;
-    
+
     if (GetSetPokedexFlag(SpeciesToNationalPokedexNum(SPECIES_DEOXYS), FLAG_GET_CAUGHT))
         numSeen--;
     if (GetSetPokedexFlag(SpeciesToNationalPokedexNum(SPECIES_JIRACHI), FLAG_GET_CAUGHT))
